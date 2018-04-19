f08442a253f3a92c93c2d728941863184c1891e8
Daniel Moser <danielmoser49@gmail.com>
2018-04-19 16:13:50 -0500

    connect front and back end

---
+++ client/src/components/Header.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+
+class Header extends Component {
+  renderContent() {
+    console.log(this.props);
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        return (
+          <li>
+            <a href="/auth/google">Login With Google</a>
+          </li>
+        );
+      default:
+        return (
+          <li>
+            <a href="/api/logout">Logout</a>
+          </li>
+        );
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <nav>
+          <div className="nav-wrapper">
+            <Link
+              to={this.props.auth ? 'surveys' : '/'}
+              className="left brand-logo"
+            >
+              Emaily
+            </Link>
+            <ul id="nav-mobile" className="right hide-on-med-and-down">
+              {this.renderContent()}
+            </ul>
+          </div>
+        </nav>
+      </div>
+    );
+  }
+}
+
+function mapStateToProps({ auth }) {
+  return { auth };
+}
+
+export default connect(mapStateToProps)(Header);
